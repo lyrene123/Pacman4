bc58fe7e46544c987f3e9473f5b65d1d32f8fbe6
DanielCavalcanti <danielhbc@gmail.com>
2017-02-14 22:32:08 -0500

    First Commit. Initializing GitHub for Pacman Project

---
+++ PacmanLibrary/Tile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace PacmanLibrary
+{
+    public class Tile
+    {
+        public Vector2 position;
+
+        public Tile(int x, int y)
+        {
+            position.X = x;
+            position.Y = y;
+        }
+       
+        
+    }
+
+}
