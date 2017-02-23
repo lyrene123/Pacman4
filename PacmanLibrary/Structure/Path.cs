cf39d307796d5039a6d44fe16db6ba3e8ee1626d
1433278 <1433278@2F18-04>
2017-02-23 16:12:07 -0500

    Path created

---
+++ PacmanLibrary/Structure/Path.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacmanLibrary.Structure
+{
+    public class Path
+    {
+
+    }
+}
