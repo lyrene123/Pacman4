112e27bfa1bdf0e3e0c36670c79431e5e3c3b0a3
Lyrene Labor <Lyrene Labor@LYRENE-PC>
2017-02-23 16:32:49 -0500

    more Maze details

--- PacmanLibrary/Structure/Maze.cs
+++ PacmanLibrary/Structure/Maze.cs
@@ -14,7 +14,8 @@ namespace PacmanLibrary.Structure
         public event PacmanWonEventHandler PacmanWonEvent;
         public Maze()
         {
-            //??????????????
+            Tile[,] someTiles = new Tile[2, 2]; //default
+            SetTiles(someTiles);
         }
 
         public void SetTiles(Tile[,] tiles)
@@ -35,18 +36,39 @@ namespace PacmanLibrary.Structure
 
         public int Size
         {
-            get { return this.maze.Length; }
+            get { return this.maze.GetLength(0); }
         }
 
         public List<Tile> GetAvailableNeighbours(Vector2 position, Direction direction)
         {
             List<Tile> availables = new List<Tile>();
-            if(direction == Direction.Down)
+            if (direction == Direction.Down)
             {
-                float posY = position.Y - 1;
-                //if(this.maze[position.X, posY] is Path)
+                availables = GetAvailableTilesDown(position);
             }
+
+            return availables;
+               
         }
+
+        private List<Tile> GetAvailableTilesDown(Vector2 position)
+        {
+            List<Tile> downTiles = new List<Tile>();
+            int posY = (int)(position.Y + 1);
+            int posX = (int)position.X; //does not change
+
+            while(posY < this.maze.GetLength(0))
+            {
+                if(this.maze[posX, posY] is Path)
+                {
+                    downTiles.Add(this.maze[posX, posY]);
+                }
+                posY++;
+            }
+
+            return downTiles;
+        }
+    }
 
 
     }
