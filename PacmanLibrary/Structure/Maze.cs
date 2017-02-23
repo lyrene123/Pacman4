e139ff10159b0ebec2c0ef08826cdcbcbec21b64
Lyrene Labor <Lyrene Labor@LYRENE-PC>
2017-02-23 17:41:42 -0500

    Maze class done for now...

--- PacmanLibrary/Structure/Maze.cs
+++ PacmanLibrary/Structure/Maze.cs
@@ -45,11 +45,51 @@ namespace PacmanLibrary.Structure
             if (direction == Direction.Down)
             {
                 availables = GetAvailableTilesDown(position);
+                availables.AddRange(GetAvailableTilesLeft(position));
+                availables.AddRange(GetAvailableTilesRight(position));
             }
 
+            if(direction == Direction.Up)
+            {
+                availables = GetAvailableTilesUp(position);
+                availables.AddRange(GetAvailableTilesLeft(position));
+                availables.AddRange(GetAvailableTilesRight(position));
+            }
+
+            if(direction == Direction.Left)
+            {
+                availables = GetAvailableTilesLeft(position);
+                availables.AddRange(GetAvailableTilesUp(position));
+                availables.AddRange(GetAvailableTilesDown(position));
+            }
+
+            if(direction == Direction.Right)
+            {
+                availables = GetAvailableTilesRight(position);
+                availables.AddRange(GetAvailableTilesUp(position));
+                availables.AddRange(GetAvailableTilesDown(position));
+            }
             return availables;
                
         }
+
+        public void CheckMembersLeft()
+        {
+            int count = 0;
+            foreach(Tile item in this.maze)
+            {
+                if (item is Path && item.IsEmpty() == false)
+                {
+                    count++;
+                }
+            }
+
+            if(count == 0)
+            {
+                PacmanWon();
+            }
+        }
+
 
         private List<Tile> GetAvailableTilesDown(Vector2 position)
         {
@@ -63,13 +103,82 @@ namespace PacmanLibrary.Structure
                 {
                     downTiles.Add(this.maze[posX, posY]);
                 }
+                else
+                {
+                    break;
+                }
+                
                 posY++;
             }
-
             return downTiles;
         }
-    }
+
+        private List<Tile> GetAvailableTilesUp(Vector2 position)
+        {
+            List<Tile> upTiles = new List<Tile>();
+            int posY = (int)(position.Y - 1);
+            int posX = (int)position.X; //does not change
+
+            while(posY >= 0)
+            {
+                if (this.maze[posX, posY] is Path)
+                {
+                    upTiles.Add(this.maze[posX, posY]);
+                }
+                else
+                {
+                    break;
+                }
+                posY--;
+            }
+            return upTiles;
+        }
+
+        private List<Tile> GetAvailableTilesLeft(Vector2 position)
+        {
+            List<Tile> leftTiles = new List<Tile>();
+            int posY = (int)position.Y; //never changes
+            int posX = (int)(position.X-1); 
+
+            while (posX >= 0)
+            {
+                if (this.maze[posX, posY] is Path)
+                {
+                    leftTiles.Add(this.maze[posX, posY]);
+                }
+                else
+                {
+                    break;
+                }
+                posY--;
+            }
+            return leftTiles;
+        }
+
+        private List<Tile> GetAvailableTilesRight(Vector2 position)
+        {
+            List<Tile> rightTiles = new List<Tile>();
+            int posY = (int)position.Y; //never changes
+            int posX = (int)(position.X + 1);
+
+            while (posX < this.maze.GetLength(1))
+            {
+                if (this.maze[posX, posY] is Path)
+                {
+                    rightTiles.Add(this.maze[posX, posY]);
+                }
+                else
+                {
+                    break;
+                }
+                posY++;
+            }
+            return rightTiles;
+        }
 
 
     }
+
+
+    
 }
