5c8c5b9a84ea8709ae2557cba9828c0ba2474ad6
1433278 <1433278@2F18-04>
2017-02-24 08:57:13 -0500

    Commite

---
+++ PacmanLibrary/Structure/Energizer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacmanLibrary.Structure
+{
+    public delegate 
+    public class Energizer : ICollidable
+    {
+        private int points;
+        //private GhostPack ghosts; // Class GhostPack  still to be created
+
+        /*public Energizer(GhostPack ghosts) // Class GhostPack  still to be created
+        {
+            this.ghosts = ghosts;
+        }
+        */
+        public int Points
+        {
+            get { return points; }
+            set { points = value;}
+        }
+
+        public void Collide()
+        {
+            this.points += 30;
+            //Invoke ScareGhosts() method from GhostPack?
+        }
+    }
+}
---
+++ PacmanLibrary/Structure/Pellet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacmanLibrary.Structure
+{
+    public class Pellet : ICollidable
+    {
+        private int points;
+        public int Points
+        {
+                      
+            get { return points; }
+            set { points = value; }
+        }
+
+        public void Collide()
+        {
+            this.points += 30;
+        }
+    }
+}
