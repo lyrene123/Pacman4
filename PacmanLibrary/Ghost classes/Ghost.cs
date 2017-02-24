38ab82bce2e6aceb5a0566f58a44b0d6de7a79e1
Lyrene Labor <Lyrene Labor@LYRENE-PC>
2017-02-24 15:52:49 -0500

    committt

--- PacmanLibrary/Ghost classes/Ghost.cs
+++ PacmanLibrary/Ghost classes/Ghost.cs
@@ -9,6 +9,7 @@ using PacmanLibrary.Structure;
 
 namespace PacmanLibrary.Ghost_classes
 {
+    public delegate void PacmanDiedEventHandler();
     /// <summary>
     /// 
     /// </summary>
@@ -22,11 +23,13 @@ namespace PacmanLibrary.Ghost_classes
         private Direction direction;
         private Color colour;
         private IGhostState currentState;
+        private int points;
 
         public static Timer scared;
         public event CollisionEventHandler CollisionEvent;
+        public event PacmanDiedEventHandler PacmanDiedEvent;
 
-       public Ghost(GameState g, int x, int y, Vector2 target, GhostState start, Color c)
+        public Ghost(GameState g, int x, int y, Vector2 target, GhostState start, Color c)
         {     
             this.pacman = g.pacmanObj;
             this.maze = g.mazeObj;
@@ -38,57 +41,86 @@ namespace PacmanLibrary.Ghost_classes
             if (start == GhostState.Scared)
                 this.currentState = new Scared(this, this.maze);
             if (start == GhostState.Chasing)
+                this.currentState = new Chase(this, this.maze, this.target, this.pacman);
 
+            this.points = 200;
+        }
 
+        protected virtual void OnCollisionEvent(ICollidable member)
+        {
+            CollisionEvent?.Invoke(member);
+        }
 
+        protected virtual void OnPacmanDiedEvent()
+        {
+            PacmanDiedEvent?.Invoke();
+            Reset();
         }
 
 
         public Direction Direction
         {
-            get
-            {
-                throw new NotImplementedException();
-            }
-
-            set
-            {
-                throw new NotImplementedException();
-            }
+            get { return this.direction; }
+            set { this.direction = value; }
         }
 
         public Vector2 Position
         {
             get{ return this.position; }
-
             set{ this.position = value; }
         }
 
         public int Points
         {
-            get
-            {
-                throw new NotImplementedException();
-            }
-
-            set
-            {
-                throw new NotImplementedException();
-            }
+            get{ return this.points; }
+            set{ this.points = value; }
         }
 
         public void Move()
         {
-            throw new NotImplementedException();
+            this.currentState.Move();
         }
 
         public void Collide()
         {
-            throw new NotImplementedException();
+           if(this.CurrentState == GhostState.Scared)
+            {
+                OnCollisionEvent(this);
+            }
+
+           if(this.CurrentState == GhostState.Chasing)
+            {
+                OnPacmanDiedEvent();
+            }
+        }
+
+        public void Reset()
+        {
+
+        }
+
+        public GhostState CurrentState
+        {
+            get
+            {
+                if (this.currentState is Scared)
+                    return GhostState.Scared;
+
+                return GhostState.Chasing;
+            }
+        }
+
+        public Color ghostColor
+        {
+            get { return this.colour;  }
         }
 
         public void ChangeState(GhostState g)
         {
+            if (g == GhostState.Scared)
+                this.currentState = new Scared(this, this.maze);
+            if (g == GhostState.Chasing)
+                this.currentState = new Chase(this, this.maze, this.target, this.pacman);
 
         }
     }
