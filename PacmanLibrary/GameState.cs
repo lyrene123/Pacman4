898e20d8255bd917ca2868e699e4a83b265d729b
DanielCavalcanti <danielhbc@gmail.com>
2017-02-26 11:18:12 -0500

    GameState updated (still need to be checked)

--- PacmanLibrary/GameState.cs
+++ PacmanLibrary/GameState.cs
@@ -49,10 +49,65 @@ namespace PacmanLibrary
                 Console.WriteLine("Could Not Find File");
             }
 
-            // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-           // assingning elements to the properties - to be implemented
-                
+            /*// On going Implementation / 
+             
+            for (int i = 0; i < map.GetLength(0); i++)
+              {
+                  for (int j = 0; j < map.GetLength(0); j++)
+                  {
+                      switch (map[i,j])
+                      {
+                          case "w":
+                              Wall wall = new Wall(i, j);
+                              g.Maze[i, j] = wall;
+                              break;
+                        case "m":
+                            Structure.Path emptyMazePath = new Structure.Path(i,j,null);
+                            g.Maze[i, j] = emptyMazePath;
+                            break;
+                        case "p":
+                              Pellet pellet = new Pellet();
+                              Structure.Path pelletMazePath = new Structure.Path(i, j, pellet);
+                              g.Maze[i, j] = pelletMazePath;
+                              break;
+                         case "pen": ////////////////////////// double check here 
+                              Pen pen = new Pen();
+                              g.Maze[i, j] = pen;
+                              break;
+                          case "e":
+                              Energizer energizer = new Energizer(g.GhostPack);
+                              Structure.Path energizerPath = new Structure.Path(i, j, energizer);
+                              g.Maze[i, j] = energizerPath;
+                              break;
+                          case "1":
+                              Vector2 blinky_target = new Vector2(g.Pacman.Position.X + 2, g.Pacman.Position.Y);
+                              g.GhostPack.Add(new Ghost(g, i, j, blinky_target, GhostState.Penned, Color.Red));
+                              break;
+                          case "2":
+                              Vector2 pinky_target = new Vector2(g.Pacman.Position.X + 4, g.Pacman.Position.Y);
+                              g.GhostPack.Add(new Ghost(g, i, j, pinky_target, GhostState.Penned, Color.Pink));
+                              break;
+                          case "3":
+                              Vector2 inky_target = new Vector2(g.Pacman.Position.X + 6, g.Pacman.Position.Y);
+                              g.GhostPack.Add(new Ghost(g, i, j, inky_target, GhostState.Penned, Color.Blue));
+                              break;
+                          case "4":
+                              Vector2 clyde_target = new Vector2(g.Pacman.Position.X + 1, g.Pacman.Position.Y);
+                              g.GhostPack.Add(new Ghost(g, i, j, clyde_target, GhostState.Penned, Color.Green));
+                              break;
+                          case "P":
+                              g.Pacman = new Pacman(g);
+                              break;
+                      }
+
+                  }
+              }*/
+
             return g;
+        }
+        private void SubscribeElements(ICollidable member)
+        {
+
         }
         public Pacman Pacman
         {
