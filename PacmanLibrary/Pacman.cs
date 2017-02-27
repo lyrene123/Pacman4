387768195bf3fa2e81a4e5690825ae2acaf1c697
Lyrene Labor <Lyrene Labor@LYRENE-PC>
2017-02-27 10:59:11 -0500

    started pacman

--- PacmanLibrary/Pacman.cs
+++ PacmanLibrary/Pacman.cs
@@ -4,12 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PacmanLibrary;
+using Microsoft.Xna.Framework;
+using PacmanLibrary.Structure;
 
 namespace PacmanLibrary
 {
     //For Amin
     public class Pacman
     {
-        /////
+        private Vector2 position;
+        private GameState gamestate;
+        private Maze maze;
+
+        public Pacman(GameState gs) 
+        {
+            this.gamestate = gs;
+            this.maze = this.gamestate.Maze;
+            this.position = this.gamestate.Pacman.position;           
+        }
+
+        public void Move(Direction dir)
+        {
+            if(dir == Direction.Down)
+            {
+
+            }
+        }
+
+        private void MoveDownIfPossible()
+
+
     }
 }
