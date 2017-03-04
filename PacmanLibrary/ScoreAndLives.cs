8ab16c84c712a6093b0d758663799276ad0e902c
DanielCavalcanti <danielhbc@gmail.com>
2017-03-03 22:04:43 -0500

    ScoreAndLives Created (On going)

---
+++ PacmanLibrary/ScoreAndLives.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacmanLibrary
+{
+    public delegate void GameOverDelegate();
+    public class ScoreAndLives
+    {
+        private GameState gameState;
+        private int lives;
+        private int score;
+        public event GameOverDelegate GameOver;
+
+        public ScoreAndLives (GameState gameState)
+        {
+            this.gameState = gameState;
+        }
+        public int Lives
+        {
+            get { return lives;  }
+            set { lives = value; }
+        }
+        public int Score
+        {
+            get { return score; }
+            set { score = value; }
+        }
+        protected void OnGameOver()
+        {
+            GameOver?.Invoke();
+        }
+
+        public void deadPacman()
+        {
+            this.Lives -= 1;
+            if(Lives == 0)
+            {
+                OnGameOver();
+            }
+        }
+        void incrementScore(ICollidable member)
+        {
+            this.Score += member.Points;
+        }
+
+    }
+}
