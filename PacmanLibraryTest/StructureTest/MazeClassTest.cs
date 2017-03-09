16e08a5f77e1c63e440520592c6d6c00bcffe4fd
Lyrene Labor <Lyrene Labor@LYRENE-PC>
2017-03-09 11:48:52 -0500

    changed to empty.csv

---
+++ PacmanLibraryTest/StructureTest/MazeClassTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PacmanLibrary;
+
+namespace PacmanLibraryTest.StructureTest
+{
+    [TestClass]
+    public class MazeClassTest
+    {
+        GameState gstate = GameState.Parse("empty");
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
