f2e000f472390686c06daa9e41286fc2aa45fe16
Lyrene Labor <Lyrene Labor@LYRENE-PC>
2017-03-10 12:35:24 -0500

    commit to sync

---
+++ PacmanLibraryTest/GhostClassesTest/ChaseClassTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PacmanLibraryTest.GhostClassesTest
+{
+    [TestClass]
+    public class ChaseClassTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
