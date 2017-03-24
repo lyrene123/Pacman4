092665bfd49fb0a26c158ae9b7ac25967086fe8e
Lyrene Labor <Lyrene Labor@LYRENE-PC>
2017-03-23 21:45:07 -0400

    commented Penned class

--- PacmanLibrary/Ghost classes/Penned.cs
+++ PacmanLibrary/Ghost classes/Penned.cs
@@ -7,10 +7,22 @@ using System.Threading.Tasks;
 
 namespace PacmanLibrary.Ghost_classes
 {
+    /// <summary>
+    /// The Penned encapsulated the behavior of a ghost when
+    /// it is inside the pen. It implements the IGhostState interface
+    /// </summary>
     public class Penned : IGhostState
     {
+        /// <summary>
+        /// No param contructor simply constructs the Penned object
+        /// </summary>
         public Penned() { }
 
+        /// <summary>
+        /// The move simply throws a NotSupportedException exception
+        /// since a ghost will normally not move inside the pen nor chase
+        /// the ghost
+        /// </summary>
         public void Move()
         {
             throw new NotSupportedException();
